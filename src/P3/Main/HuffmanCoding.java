dc6488eebfa77ceb5a6785ab8e9603ac0dd81cfd
bermed28 <bermed28@localhost>
2020-05-12 10:08:08 -0400

    Added output and fixed ties
    still need to calculate savings %, also added sample files

--- src/P3/Main/HuffmanCoding.java
+++ src/P3/Main/HuffmanCoding.java
@@ -1,9 +1,13 @@
 package P3.Main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import P3.DataStructures.List.*;
 import P3.DataStructures.Map.*;
@@ -20,18 +24,18 @@ public class HuffmanCoding {
 	}
 
 	private static void run() {
-		String data = loadData("huffman.txt");
-		Map<String, Integer> fD = computeFD(data);
-		BTNode<String, Integer> huffmanRoot = huffmanTree(fD);
-		Map<String,String> encodedHuffman = huffmanCode(huffmanRoot);
+		String data = load_data("stringData.txt");
+		Map<String, Integer> fD = compute_fd(data);
+		BTNode<String, Integer> huffmanRoot = huffman_tree(fD);
+		Map<String,String> encodedHuffman = huffman_code(huffmanRoot);
 		String output = encode(encodedHuffman, data);
-		processResults(fD,data,output);
+		process_results(fD, encodedHuffman,data,output);
 
 	}
 
 
 
-	public static String loadData(String inputFile) {
+	public static String load_data(String inputFile) {
 		BufferedReader br = null;
 		String line = "";
 
@@ -60,7 +64,7 @@ public class HuffmanCoding {
 		return line;
 	}
 
-	public static Map<String, Integer> computeFD(String data) {
+	public static Map<String, Integer> compute_fd(String data) {
 		Map<String,Integer> ht = new HashTableSC<String,Integer>(data.length(), new SimpleHashFunction<String>());
 		for (int i = 0; i < data.length(); i++) {
 			char key = data.charAt(i);
@@ -74,7 +78,7 @@ public class HuffmanCoding {
 		return ht;
 	}
 
-	public static BTNode<String, Integer> huffmanTree(Map<String, Integer> fD) {
+	public static BTNode<String, Integer> huffman_tree(Map<String, Integer> fD) {
 		int size = fD.size();
 		boolean isEmpty =true;
 		SortedList<BTNode<String,Integer>> sL = new SortedLinkedList<BTNode<String,Integer>>();
@@ -91,28 +95,23 @@ public class HuffmanCoding {
 
 			BTNode<String, Integer> x = minFreq(sL);
 			BTNode<String, Integer> y = minFreq(sL);
-			
-			if(isEmpty) {
+
+
+			if(x.getValue() == y.getValue() && y.getKey().compareTo(x.getKey()) < 0) {
+				n.setLeftChild(y);
+				n.setRightChild(x);
+
+			} else {
 				n.setLeftChild(x);
 				n.setRightChild(y);
-				isEmpty = false;
-			}else {
-				if(x.getValue() == y.getValue() && x.getKey().compareTo(y.getKey()) < 0) {
-					n.setLeftChild(y);
-					n.setRightChild(x);
-	
-				} else {
-					n.setLeftChild(x);
-					n.setRightChild(y);
-				}
 			}
+
 			n.setValue(x.getValue() + y.getValue());
 			n.setKey(x.getKey() + y.getKey());
 			sL.add(n);
 		}
 
 		BTNode<String, Integer> nodeToReturn = minFreq(sL);
-		System.out.println("Root of Huffman Tree: {" + nodeToReturn.getKey() + "," + nodeToReturn.getValue()+"}");
 		System.out.println("----------------------------");
 		System.out.println("Huffman Tree: ");
 		BinaryTreePrinter.print(nodeToReturn);
@@ -143,43 +142,48 @@ public class HuffmanCoding {
 		return smallNode;
 	}
 
-	public static Map<String, String> huffmanCode(BTNode<String, Integer> huffmanRoot) {
+	public static Map<String, String> huffman_code(BTNode<String, Integer> huffmanRoot) {
 		Map<String, String> ht = new HashTableSC<String, String>(new SimpleHashFunction<String>());
 		Stack<String> stack = new LinkedListStack<>();
-		ht = recTraversal(huffmanRoot, ht, stack);
+		ht = buildPrexixCodeMap(huffmanRoot, ht, stack);
 		return ht;
 	}
 
-	private static Map<String,String> recTraversal(BTNode<String, Integer> huffmanRoot, Map<String, String> ht, Stack<String> s) {
+	private static Map<String,String> buildPrexixCodeMap(BTNode<String, Integer> huffmanRoot, Map<String, String> ht, Stack<String> s) {
 		if(huffmanRoot == null) return ht;
 		else {
 			if(isLeaf(huffmanRoot)) {
 				Stack<String> temp = new LinkedListStack<>();
 				String encode = "";
-				while (!s.isEmpty()) {
-					String popped = s.pop();
-					encode = popped + encode;
-					temp.push(popped);
-				}
-				while(!temp.isEmpty()) s.push(temp.pop());
-
+				encode = generatePrefixCode(encode,s, temp);
 				ht.put(huffmanRoot.getKey(), encode);
 			} 
 			s.push("0");
-			recTraversal(huffmanRoot.getLeftChild(), ht,s);
+			buildPrexixCodeMap(huffmanRoot.getLeftChild(), ht,s);
 			s.pop();
 
 			s.push("1");
-			recTraversal(huffmanRoot.getRightChild(), ht,s);
+			buildPrexixCodeMap(huffmanRoot.getRightChild(), ht,s);
 			s.pop();
 		}
-
+		
 		return ht;
+	}
+
+	private static String generatePrefixCode(String encode, Stack<String> s, Stack<String> temp) {
+		while (!s.isEmpty()) {
+			String popped = s.pop();
+			encode = popped + encode;
+			temp.push(popped);
+		}
+		while(!temp.isEmpty()) s.push(temp.pop());
+		return encode;
 	}
 
 	private static boolean isLeaf(BTNode<String, Integer> huffmanRoot) {
 		return huffmanRoot.getLeftChild() == null && huffmanRoot.getRightChild() == null;
 	}
+
 	public static String encode(Map<String, String> encodedHuffman, String inputString) {
 		String result = "";
 
@@ -189,22 +193,72 @@ public class HuffmanCoding {
 				result += encodedHuffman.get(letter);			
 
 		}
-		
+
 		return result;
 	}
 
 
-	public static void processResults(Map<String, Integer> fD, String inputData, String outputData) {
+	public static void process_results(Map<String, Integer> fD, Map<String, String> encodedHuffman, String inputData, String outputData) {
 		int bytes = 0;
-		int savings = 0;
-		System.out.println("----------------------------");
-		System.out.println("Frequency Distribution of " + inputData + " is: ");
-		fD.print(System.out);		
-		System.out.println("----------------------------");
-		System.out.println("Original Text: " + inputData);
-		System.out.println("Encoded Text: " + outputData);
-		System.out.println("----------------------------");
-		System.out.println("Amount of bytes needed to store the encoded text: " + bytes);
-		System.out.println("Savings percentage compared to the amount of bytes needed for the original text: " + savings + "%");
+		int encodedBytes = 0;
+		double savings = 0.0;
+		int size = fD.size() - 1;
+		List<String> letters = fD.getKeys();
+		List<Integer> frequencies = fD.getValues();
+		List<String> prefixCodes = encodedHuffman.getValues();
+		
+		System.out.println("Symbol" + "\t\t" + "Frequency" + '\t' + "Code");
+		System.out.println("------" + "\t\t" + "---------" + "\t" + "----");
+		for (int i = size; i >= 0; i--) {
+			System.out.println(letters.get(i) + "\t\t" + frequencies.get(i) + "\t\t" + prefixCodes.get(i));
+		}
+		System.out.println();
+		
+		System.out.println("Original String: \n" + inputData);
+		System.out.println("Encoded String: \n" + outputData + '\n');
+		System.out.println("The original string requires " + bytes + " bytes");
+		System.out.println("The encoded string requires " + encodedBytes + " bytes");
+		System.out.println("Difference in space required is " + savings + "%");
+		
+		processOutputFile(inputData, outputData, letters, frequencies, prefixCodes, bytes, encodedBytes, savings);
+	}
+
+	private static void processOutputFile(String inputData, String outputData, List<String> letters, 
+										  List<Integer> frequencies, List<String> prefixCodes,
+										  int bytes, int encodedBytes, double savings) {
+		int size = letters.size() - 1;
+		Writer writer = null;
+		try {	
+
+			writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream("huffmanResults.txt"), "utf-8"));
+			
+			writer.write("Symbol" + "\t\t" + "Frequency" + '\t' + "Code\n");
+			writer.write("------" + "\t\t" + "---------" + "\t" + "----\n");
+			for (int i = size; i >= 0; i--) {
+				writer.write(letters.get(i) + "\t\t\t" + frequencies.get(i) + "\t\t\t" + prefixCodes.get(i) + '\n');
+			}
+			writer.write('\n');
+			
+			writer.write("Original String: \n" + inputData + '\n');
+			writer.write("Encoded String: \n" + outputData + "\n\n");
+			writer.write("The original string requires " + bytes + " bytes\n");
+			writer.write("The encoded string requires " + encodedBytes + " bytes\n");
+			writer.write("Difference in space required is " + savings + "%");
+			
+			
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(writer != null) {
+					writer.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
